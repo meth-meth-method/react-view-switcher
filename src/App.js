45c8f51bccc1d2633f49025a1961a9f07bd58e71
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-09 19:20:13 +0100

    Import mock content and add some to ViewSwitcher.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Image, AboutHorse} from './MockContent';
 import {ViewSwitcher} from './ViewSwitcher';
 
 class App extends Component {
@@ -12,7 +13,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <ViewSwitcher/>
+        <ViewSwitcher>
+          <Image url="https://i.imgur.com/QlsmNE1.jpg"/>
+          <AboutHorse/>
+          <Image url="https://i.imgur.com/wYTCtRu.jpg"/>
+          <Image url="https://i.imgur.com/Zu0bhYu.jpg"/>
+        </ViewSwitcher>
       </div>
     );
   }
