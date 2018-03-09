03b7c8ab052ed4b54b3a07f134208214e933db06
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-09 19:20:55 +0100

    Render children in ViewSwitcher.

--- src/ViewSwitcher/index.jsx
+++ src/ViewSwitcher/index.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class ViewSwitcher extends Component {
   render() {
     return <div className="ViewSwitcher">
-      This is the view switcher root
+      {this.props.children}
     </div>;
   }
 }
