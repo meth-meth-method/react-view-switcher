85135ad642714b07e83b57c2073b7d8dbcd3425b
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-09 19:31:14 +0100

    Add controls to App.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,41 @@ import {Image, AboutHorse} from './MockContent';
 import {ViewSwitcher} from './ViewSwitcher';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: 0,
+    };
+  }
+
+  skip(offset) {
+    this.setState(prevState => {
+      return {
+        active: prevState.active + offset,
+      };
+    });
+  }
+
+  prev = () => {
+    this.skip(-1);
+  }
+
+  next = () => {
+    this.skip(1);
+  }
+
   render() {
+    const {active} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <button onClick={this.prev}>Prev</button>
+          {active}
+          <button onClick={this.next}>Next</button>
         </header>
-        <ViewSwitcher active={2}>
+        <ViewSwitcher active={active}>
           <Image url="https://i.imgur.com/QlsmNE1.jpg"/>
           <AboutHorse/>
           <Image url="https://i.imgur.com/wYTCtRu.jpg"/>
