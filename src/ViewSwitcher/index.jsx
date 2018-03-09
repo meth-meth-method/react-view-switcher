b78fdcbf408a65462ea6915c01b6c1b096cfb57b
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-09 17:52:14 +0100

    Add ViewSwitcher placeholder.# Please enter the commit message for your changes. Lines starting

---
+++ src/ViewSwitcher/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class ViewSwitcher extends Component {
+  render() {
+    return <div className="ViewSwitcher">
+      This is the view switcher root
+    </div>;
+  }
+}
